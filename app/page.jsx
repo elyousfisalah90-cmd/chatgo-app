b262fac3e340d9def79a0942d90a86acc9e41403
Salah el yousfi <elyousfisalah90@gmail.com>
2026-04-13 04:38:48 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,24 @@
-<button
-  onClick={() => alert('مرحبا بك ف ChatGo 💬')}
-  style={{
-    marginTop: "20px",
-    padding: "12px 25px",
-    borderRadius: "12px",
-    border: "none",
-    background: "#22c55e",
-    color: "white",
-    fontSize: "16px",
-    cursor: "pointer"
-  }}
->
-  دخول
-</button>
+"use client";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+
+  return (
+    <button
+      onClick={() => router.push("/chat")}
+      style={{
+        marginTop: "20px",
+        padding: "12px 25px",
+        borderRadius: "12px",
+        border: "none",
+        background: "#22c55e",
+        color: "white",
+        fontSize: "16px",
+        cursor: "pointer"
+      }}
+    >
+      دخول
+    </button>
+  );
+}
