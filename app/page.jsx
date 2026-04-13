89cb1848caeb15a9f01b8b1464394048f83a2461
Salah el yousfi <elyousfisalah90@gmail.com>
2026-04-13 04:58:03 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,39 @@
 "use client";
+
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
 
   return (
-    <button
-      onClick={() => router.push("/chat")}
-      style={{
-        marginTop: "20px",
-        padding: "12px 25px",
-        borderRadius: "12px",
-        border: "none",
-        background: "#22c55e",
-        color: "white",
-        fontSize: "16px",
-        cursor: "pointer"
-      }}
-    >
-      دخول
-    </button>
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      flexDirection: "column",
+      backgroundColor: "#0f172a",
+      color: "white"
+    }}>
+      
+      <h1>🚀 ChatGo</h1>
+
+      <button
+        onClick={() => router.push("/chat")}
+        style={{
+          marginTop: "20px",
+          padding: "12px 25px",
+          borderRadius: "12px",
+          border: "none",
+          background: "#22c55e",
+          color: "white",
+          fontSize: "16px",
+          cursor: "pointer"
+        }}
+      >
+        دخول
+      </button>
+
+    </div>
   );
 }
